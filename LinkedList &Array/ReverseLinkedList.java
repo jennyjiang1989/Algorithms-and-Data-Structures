3a04802a0b8d9a18201e066601549959a13c17ba
Jenny Jiang <jiangchunxing@gmail.com>
2019-06-25 22:49:38 -0700

    Create ReverseLinkedList.java

---
+++ LinkedList &Array/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+//   1->2->3->4->N
+//N<-1<-2<-3<-4
+public class ReverseLinkedList {
+	public ListNode reverse(ListNode head){
+		//删除节点的时候也需要prev指针跟curr指针
+		ListNode prev=null;
+		ListNode curr=head;
+		while(curr!=null){
+			ListNode temp=curr.next;
+			curr.next=prev;
+			//移动
+			prev=curr;
+			curr=temp;
+		}
+		return prev;//此时curr=null		
+	}
