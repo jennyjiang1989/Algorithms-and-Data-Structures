d3266e73ca944845accb17b574713ef10471575a
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-01 17:02:18 -0700

    Update NthUglyNumber.java

--- HashMap & Heap/NthUglyNumber.java
+++ HashMap & Heap/NthUglyNumber.java
@@ -1,13 +1,15 @@
 //Ugly number is a number that only have factors 2, 3 and 5.
 //Design an algorithm to find the nth ugly number. The first 10 ugly numbers are 1, 2, 3, 4, 5, 6, 8, 9, 10, 12...
 import java.util.*;
-public class UglyNumbers {
-	public int nthUglyNumber(int n) {
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: return a  integer as description.
+     */
+    public int nthUglyNumber(int n) {
         // write your code here
-		//O(nlogn)
         PriorityQueue<Long> pq=new PriorityQueue<>();
         HashSet<Long> set=new HashSet<>();
-        
         long[] primes=new long[3];
         primes[0]=(long)2;
         primes[1]=(long)3;
@@ -17,8 +19,7 @@ public class UglyNumbers {
             pq.add(primes[i]);
             set.add(primes[i]);
         }
-        
-        long number=(long)1;//first
+        long number=(long)1;
         for(int i=1;i<n;i++){
             number=pq.poll();
             for(int j=0;j<3;j++){
