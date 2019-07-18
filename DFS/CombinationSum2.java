b048e947245c2ce3f3d16a675cefb5e78e6b7d6d
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-18 16:05:16 -0700

    Update CombinationSum2.java

--- DFS/CombinationSum2.java
+++ DFS/CombinationSum2.java
@@ -1,6 +1,15 @@
 //Given a collection of candidate numbers (candidates) and a target number (target), 
 //find all unique combinations in candidates where the candidate numbers sums to target.
 //Each number in candidates may only be used once in the combination.
+//Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> results=new ArrayList<>();
