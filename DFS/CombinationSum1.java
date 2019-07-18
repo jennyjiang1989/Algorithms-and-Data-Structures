37933c1e673a156ab70962d390b929d6ea9a84af
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-18 16:15:30 -0700

    Update CombinationSum1.java

--- DFS/CombinationSum1.java
+++ DFS/CombinationSum1.java
@@ -1,4 +1,4 @@
-//Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), 
+//Given a set of candidate numbers (candidates) (without duplicates数组本身没有重复！) and a target number (target), 
 //find all unique combinations in candidates where the candidate numbers sums to target.
 //The same repeated number may be chosen from candidates unlimited number of times.
 
