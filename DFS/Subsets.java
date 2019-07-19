fd9835bb85bb767d59db9161117b5ac93068e01e
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-19 15:08:00 -0700

    Update Subsets.java

--- DFS/Subsets.java
+++ DFS/Subsets.java
@@ -30,3 +30,29 @@ class Solution {
     //3. 递归的出口
     // return;
 }
+//非递归算法
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> results=new ArrayList<>();
+        int n=nums.length;
+        Arrays.sort(nums);
+        //1<<n is 2^n
+        //each subset equals to an binary integer between 0 .. 2^n-1
+        //0 -> 000 -> []
+        //1 -> 001 -> [1]
+        //2 -> 010 -> [2]
+        //..
+        //7 -> 111 -> [1,2,3]
+        for(int i=0;i<(1<<n);i++){
+            ArrayList<Integer> subset=new ArrayList<>();
+            for(int j=0;j<n;j++){
+                //check whether the jth digit in i's binary representation is 1
+                if((i&(1<<j))!=0){
+                    subset.add(nums[j]);
+                }
+            }
+            results.add(subset);
+        }
+        return results;
+    }
+}
