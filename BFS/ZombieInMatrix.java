8cfee5bb9f075b9308e6ccfe76d5570bca23a12f
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-19 16:27:05 -0700

    Create ZombieInMatrix.java

---
+++ BFS/ZombieInMatrix.java
@@ -0,0 +1,7 @@
+//Given a 2D grid, each cell is either a wall 2, a zombie 1 or people 0. 
+//Zombies can turn the nearest people into zombies every day, but can't through wall.
+//How long will it take to turn all people into zombies? Return -1 if can not turn all people into zombies.
+
+public class Solution{
+
+}
