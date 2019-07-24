69bd2c2bbe1d7938574ffabb2f4424578cac1319
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-24 11:32:07 -0700

    Create ValidPalindrome.java

---
+++ Two Pointers/ValidPalindrome.java
@@ -0,0 +1,29 @@
+//相向双指针
+//Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+public class Solution {
+    /**
+     * @param s: A string
+     * @return: Whether the string is a valid palindrome
+     */
+    public boolean isPalindrome(String s) {
+        // write your code here
+        if(s==null||s.length()==0){
+            return true;
+        }
+        int i=0,j=s.length()-1;
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
