b075cf252b2277cd6ea2504875393a94321187f5
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-24 10:36:45 -0700

    Create RemoveDuplicateNumbersInArray.java

---
+++ Two Pointers/RemoveDuplicateNumbersInArray.java
@@ -0,0 +1,22 @@
+//Given an array of integers, remove the duplicate numbers in it.
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: the number of unique integers
+     */
+    public int deduplication(int[] nums) {
+        // write your code here
+        if(nums==null||nums.length==0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
