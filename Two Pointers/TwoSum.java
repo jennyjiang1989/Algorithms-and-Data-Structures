c40048ce12ed3592b08ec647d4c7b50b9fdefdd8
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-25 11:47:38 -0700

    Update TwoSum.java

--- Two Pointers/TwoSum.java
+++ Two Pointers/TwoSum.java
@@ -58,3 +58,45 @@ public class Solution {
         return result;
     }
 }
+
+//Given an array of integers, find how many unique pairs in the array such that their sum is equal to a specific target number. 
+//Please return the number of pairs.
+public class Solution {
+    /**
+     * @param nums an array of integer
+     * @param target an integer
+     * @return an integer
+     */
+    public int twoSum6(int[] nums, int target) {
+        // Write your code here
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        
+        int count = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+                left++;
+            }
+            else if (nums[left] + nums[right] > target) {
+                right--;
+            }
+            else {
+                count++;
+                left++;
+                right--;
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+                while (left < right && nums[right] == nums[right + 1]) {
+                    right--;
+                }
+            }
+        }
+        return count;
+    }
+}
