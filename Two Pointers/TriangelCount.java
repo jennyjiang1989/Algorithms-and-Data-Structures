7b723c893fc408bf1b88982faf33a86f484f06f0
Jenny Jiang <jiangchunxing@gmail.com>
2019-07-26 14:24:44 -0700

    Create TriangelCount.java

---
+++ Two Pointers/TriangelCount.java
@@ -0,0 +1,31 @@
+//Given an array of integers, how many three numbers can be found in the array, 
+//so that we can build an triangle whose three edges length is the three numbers that we find?
+
+public class Solution {
+    /**
+     * @param S: A list of integers
+     * @return: An integer
+     */
+    //a+b>c
+    public int triangleCount(int[] S) {
+        // write your code here
+        if(S==null||S.length<3){
+            return 0;
+        }
+        Arrays.sort(S);
+        int count=0;
+        //1 2 3 4 3
+        for(int i=2;i<S.length;i++){
+            int start=0;
+            int end=i-1;
+            while(start<end){
+                if(S[start]+S[end]>S[i]){
+                    count=count+end-start;
+                }else{
+                    start++;
+                }
+            }
+        }
+        return count;
+    }
+}
