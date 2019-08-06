777980574b84bc6edb8909656bd3b8b1496a3372
Jenny Jiang <jiangchunxing@gmail.com>
2019-08-06 15:27:04 -0700

    Update SortColors.java

--- Two Pointers/SortColors.java
+++ Two Pointers/SortColors.java
@@ -32,3 +32,48 @@ public class Solution {
         nums[j]=temp;
     }
 }
+
+//Given an array of n objects with k different colors (numbered from 1 to k), 
+//sort them so that objects of the same color are adjacent, with the colors in the order 1, 2, ... k.
+public class Solution {
+    /**
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    public void sortColors2(int[] colors, int k) {
+        // write your code here
+        if(colors==null||colors.length==0){
+            return;
+        }
+        rainbowSort(colors,0,colors.length-1,1,k);
+    }
+    private void rainbowSort(int[] colors, int left, int right, int colorFrom, int colorTo){
+        if(colorFrom==colorTo){
+            return;
+        }
+        if(left>=right){
+            return;
+        }
+        int colorMid=(colorFrom+colorTo)/2;
+        int l=left,r=right;
+        while(l<=r){
+            while(l<=r && colors[l]<=colorMid){
+                l++;
+            }
+            while(l<=r && colors[r]>colorMid){
+                r--;
+            }
+            if(l<=r){
+                int temp=colors[l];
+                colors[l]=colors[r];
+                colors[r]=temp;
+                l++;
+                r--;
+            }
+        }
+        //left r l right
+        rainbowSort(colors,left,r,colorFrom,colorMid);
+        rainbowSort(colors,l,right,colorMid+1,colorTo);
+    }
+}
