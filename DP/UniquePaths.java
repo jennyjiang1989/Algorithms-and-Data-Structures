43c62ae5d9e34844bf246153c9666b3e04223b53
Jenny Jiang <jiangchunxing@gmail.com>
2019-08-14 13:22:52 -0700

    Update UniquePaths.java

--- DP/UniquePaths.java
+++ DP/UniquePaths.java
@@ -13,12 +13,11 @@ public class Solution {
             return 0;
         }
         int[][] f=new int[m][n];
-        f[0][0]=1;
-        for(int i=1;i<m;i++){
-            f[i][0]=f[i-1][0];
+        for(int i=0;i<m;i++){
+            f[i][0]=1;
         }
-        for(int j=1;j<n;j++){                                    
-            f[0][j]=f[0][j-1];
+        for(int j=0;j<n;j++){                                    
+            f[0][j]=1;
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
