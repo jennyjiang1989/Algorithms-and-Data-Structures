a9ac44b2706f55446603c1884bb7c63abd1f2380
Jenny Jiang <jiangchunxing@gmail.com>
2019-08-23 17:35:23 -0700

    Update PreorderTraversal.java

--- BinaryTree&DivideConquer/PreorderTraversal.java
+++ BinaryTree&DivideConquer/PreorderTraversal.java
@@ -9,7 +9,7 @@
  *     }
  * }
  */
-
+//非递归
 public class Solution {
     /**
      * @param root: A Tree
@@ -35,6 +35,27 @@ public class Solution {
                 stack.push(node.left);
             }
         }
+        return result;
+    }
+}
+
+//分治算法
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+        List<Integer> result=new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        result.add(root.val);
+        List<Integer> left=preorderTraversal(root.left);
+        List<Integer> right=preorderTraversal(root.right);
+        result.addAll(left);
+        result.addAll(right);
         return result;
     }
 }
