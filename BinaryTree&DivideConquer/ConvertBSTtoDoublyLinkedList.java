9b7798dc72de365ea2344dacf985896468247f2f
Jenny Jiang <jiangchunxing@gmail.com>
2019-08-26 10:18:51 -0700

    Create ConvertBSTtoDoublyLinkedList.java

---
+++ BinaryTree&DivideConquer/ConvertBSTtoDoublyLinkedList.java
@@ -0,0 +1,76 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Definition for Doubly-ListNode.
+ * public class DoublyListNode {
+ *     int val;
+ *     DoublyListNode next, prev;
+ *     DoublyListNode(int val) {
+ *         this.val = val;
+ *         this.next = this.prev = null;
+ *     }
+ * }
+ */ 
+ 
+class ResultType {
+    DoublyListNode first, last;
+    
+    public ResultType(DoublyListNode first, DoublyListNode last) {
+        this.first = first;
+        this.last = last;
+    }
+}
+
+public class Solution {
+    /**
+     * @param root: The root of tree
+     * @return: the head of doubly list node
+     */
+    public DoublyListNode bstToDoublyList(TreeNode root) {  
+        if (root == null) {
+            return null;
+        }
+        
+        ResultType result = helper(root);
+        return result.first;
+    }
+    
+    //中序遍历函数 
+    public ResultType helper(TreeNode root) {  
+        if (root == null) {
+            return null;
+        }
+        
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+        DoublyListNode node = new DoublyListNode(root.val);
+        
+        ResultType result = new ResultType(null, null);
+        
+        //构造单链表
+        if (left == null) {
+            result.first = node;
+        } else {
+            result.first = left.first;
+            left.last.next = node;
+            node.prev = left.last;
+        }
+        
+        if (right == null) {
+            result.last = node;
+        } else {
+            result.last = right.last;
+            right.first.prev = node;
+            node.next = right.first;
+        }
+        
+        return result;
+    }
+}
