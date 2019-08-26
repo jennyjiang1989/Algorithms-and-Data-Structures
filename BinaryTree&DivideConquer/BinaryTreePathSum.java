465557ff0b136edbf5af62a2677aa22e1efd3422
Jenny Jiang <jiangchunxing@gmail.com>
2019-08-26 08:12:40 -0700

    Create BinaryTreePathSum.java

---
+++ BinaryTree&DivideConquer/BinaryTreePathSum.java
@@ -0,0 +1,54 @@
+//Given a binary tree, find all paths that sum of the nodes in the path equals to a given number target.
+//A valid path is from root node to any of the leaf nodes.
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: the root of binary tree
+     * @param target: An integer
+     * @return: all valid paths
+     */
+    public List<List<Integer>> binaryTreePathSum(TreeNode root, int target) {
+        // write your code here
+        List<List<Integer>> results=new ArrayList<>();
+        if(root==null){
+            return results;
+        }
+        List<Integer> path=new ArrayList<>();
+        path.add(root.val);
+        helper(path,root,root.val,target,results);
+        return results;
+    }
+    //"sum": the sum of each node from root to current node.
+    private void helper(List<Integer> path, TreeNode root, int sum, int target, List<List<Integer>> results){
+        if(root.left==null&&root.right==null){
+            if(sum==target){
+                //wrong: results.add(path);
+                results.add(new ArrayList<Integer>(path));
+                return;
+            }
+        }
+        if(root.left!=null){
+            path.add(root.left.val);
+            helper(path,root.left,sum+root.left.val,target,results);
+            path.remove(path.size()-1);
+        }
+        if(root.right!=null){
+            path.add(root.right.val);
+            helper(path,root.right,sum+root.right.val,target,results);
+            path.remove(path.size()-1);
+        }
+    }
+}
