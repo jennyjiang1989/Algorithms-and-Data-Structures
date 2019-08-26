eb5738180ac1662414994b5d581d450c53c514df
Jenny Jiang <jiangchunxing@gmail.com>
2019-08-26 08:47:06 -0700

    Update BinaryTreeLongestConsecutiveSequence.java

--- BinaryTree&DivideConquer/BinaryTreeLongestConsecutiveSequence.java
+++ BinaryTree&DivideConquer/BinaryTreeLongestConsecutiveSequence.java
@@ -42,3 +42,49 @@ public class Solution {
         return subtreeLongest;
     }
 }
+
+//The path could be start and end at any node in the tree
+public class Solution {
+    /**
+     * @param root: the root of binary tree
+     * @return: the length of the longest consecutive sequence path
+     */
+    private class ResultType{
+        public int max_length;
+        public int max_down;
+        public int max_up;
+        public ResultType(int max_length,int max_down,int max_up){
+            this.max_length=max_length;
+            this.max_down=max_down;
+            this.max_up=max_up;
+        }
+    }
+    public int longestConsecutive2(TreeNode root) {
+        // write your code here
+        return helper(root).max_length;
+    }
+    private ResultType helper(TreeNode root){
+        if(root==null){
+            return new ResultType(0,0,0);
+        }
+        ResultType left=helper(root.left);
+        ResultType right=helper(root.right);
+        
+        int down=0,up=0;
+        if(root.left!=null&&root.val-1==root.left.val){
+            down=Math.max(down,left.max_down+1);
+        }
+        if(root.left!=null&&root.val+1==root.left.val){
+            up=Math.max(up,left.max_up+1);
+        }
+        if(root.right!=null&&root.val-1==root.right.val){
+            down=Math.max(down,right.max_down+1);
+        }
+        if(root.right!=null&&root.val+1==root.right.val){
+            up=Math.max(up,right.max_up+1);
+        }
+        int length=down+1+up;
+        length=Math.max(length,Math.max(left.max_length,right.max_length));
+        return new ResultType(length,down,up);
+    }
+}
