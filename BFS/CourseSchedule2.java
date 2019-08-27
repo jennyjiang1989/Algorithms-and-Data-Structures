9c4cab0b2a99f2930a25dbd65f08c3a0039a01cf
Jenny Jiang <jiangchunxing@gmail.com>
2019-08-27 16:39:46 -0700

    Update CourseSchedule2.java

--- BFS/CourseSchedule2.java
+++ BFS/CourseSchedule2.java
@@ -1,26 +1,31 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList[] graph = new ArrayList[numCourses];
-        int[] degree = new int[numCourses];
-        Queue<Integer> queue = new LinkedList<>();
-        ArrayList<Integer> order=new ArrayList<>();
+        ArrayList[] graph = new ArrayList[numCourses];//邻接表
+        int[] degree = new int[numCourses];//入度
+        Queue<Integer> queue = new LinkedList<>();//bfs
+        ArrayList<Integer> order=new ArrayList<>();//因为不知道是否有拓扑排序（能否完成所有的课程）
         
         for(int i=0;i<numCourses;i++)
-            graph[i] = new ArrayList();
-            
+            graph[i] = new ArrayList<Integer>();
+        //构造邻接表与统计入度同时进行   
         for(int i=0; i<prerequisites.length;i++){
-            degree[prerequisites[i][0]]++;
-            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+            degree[u]++;
+            graph[v].add(u);
         }
+        //把入度为0的点加入queue
         for(int i=0; i<degree.length;i++){
             if(degree[i] == 0){
                 queue.add(i);
             }
         }
-        while(queue.size()!= 0){
+        //bfs
+        while(!queue.isEmpty()){
             int course = queue.poll();
             order.add(course);
-            for(int i=0; i<graph[course].size();i++){
+            int size=graph[course].size();
+            for(int i=0; i<size;i++){
                 int pointer = (int)graph[course].get(i);
                 degree[pointer]--;
                 if(degree[pointer] == 0){
