2e7792d47b7276311526062c9a66da706bb172b9
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-01 14:15:03 -0700

    Update 子数组SubarraySum.java

--- LinkedList &Array/子数组SubarraySum.java
+++ LinkedList &Array/子数组SubarraySum.java
@@ -1,3 +1,5 @@
+//Given an integer array, find a subarray where the sum of numbers is zero. 
+//Your code should return the index of the first number and the index of the last number.
 public class Solution {
     /**
      * @param nums: A list of integers
