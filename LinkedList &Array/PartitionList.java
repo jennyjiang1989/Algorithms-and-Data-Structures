86267576044a573637de5ae29a7312d7b3a7f1e8
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-01 20:10:07 -0700

    Update PartitionList.java

--- LinkedList &Array/PartitionList.java
+++ LinkedList &Array/PartitionList.java
@@ -1,5 +1,6 @@
 //Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
 //You should preserve the original relative order of the nodes in each of the two partitions.
+//idea：用两个list分别存小于x和大于x的，然后连接起来
 /**
  * Definition for ListNode
  * public class ListNode {
@@ -23,11 +24,11 @@ public class Solution {
         if(head==null){
             return null;
         }
-        ListNode dummy1=new ListNode(0);
-        ListNode dummy2=new ListNode(0);
-        ListNode node=head;
-        ListNode node1=dummy1;
-        ListNode node2=dummy2;
+        ListNode dummy1=new ListNode(0);//list1的dummy node
+        ListNode dummy2=new ListNode(0);//list2的dummy node
+        ListNode node=head;//用于遍历
+        ListNode node1=dummy1;//用于遍历
+        ListNode node2=dummy2;//用于遍历
         while(node!=null){
             if(node.val<x){
                 node1.next=new ListNode(node.val);
