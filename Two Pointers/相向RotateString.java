bb97dfe5b9fc488ef898fbd712e5eaab6ac7c390
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-02 17:41:36 -0700

    Create 相向RotateString.java

---
+++ Two Pointers/相向RotateString.java
@@ -0,0 +1,28 @@
+//Given a string(Given in the way of char array) and an offset, rotate the string by offset in place. (rotate from left to right).
+//Input: str="abcdefg", offset = 3 Output: str = "efgabcd"	
+
+public class Solution {
+    /**
+     * @param str: an array of char
+     * @param offset: an integer
+     * @return: nothing
+     */
+    public void rotateString(char[] str, int offset) {
+        // write your code here
+        if (str == null || str.length == 0)
+            return;
+            
+        offset = offset % str.length;
+        reverse(str, 0, str.length - offset - 1);
+        reverse(str, str.length - offset, str.length - 1);
+        reverse(str, 0, str.length - 1);
+    }
+    
+    private void reverse(char[] str, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+        }
+    }
+}
