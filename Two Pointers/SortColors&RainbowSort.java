1e7514335d9585c8312b40b954f9bfe24a6c7626
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-05 10:48:33 -0700

    Update SortColors&RainbowSort.java

--- Two Pointers/SortColors&RainbowSort.java
+++ Two Pointers/SortColors&RainbowSort.java
@@ -1,4 +1,5 @@
-//Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+//Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, 
+//with the colors in the order red, white and blue.
 
 public class Solution {
     /**
