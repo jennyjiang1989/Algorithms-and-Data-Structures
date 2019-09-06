fea7639a418c8497af009e046154f66aecafdb10
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-05 19:42:09 -0700

    Update LRUCache.java

--- HashMap & Heap/LRUCache.java
+++ HashMap & Heap/LRUCache.java
@@ -5,61 +5,72 @@
 //Doubly linked list + HashMap
 import java.util.*;
 public class LRUCache {
-	private class Node{
-		Node prev;
-		Node next;
-		int key;
-		int value;
-		public Node(int key, int value){
-			this.key=key;
-			this.value=value;
-			this.prev=null;
-			this.next=null;
-		}
-	}
-	
-	private int capacity;
-	private HashMap<Integer,Node> map=new HashMap<>();
-	private Node head=new Node(-1,-1);
-	private Node tail=new Node(-1,-1);
-	
-	public LRUCache(int capacity){
-		this.capacity=capacity;
-		tail.prev=head;
-		head.next=tail;
-	}
-	public int get(int key){
-		if(!map.containsKey(key)){
-			return -1;
-		}
-		//remove current
-		Node current=map.get(key);
-		current.prev.next=current.next;
-		current.next.prev=current.prev;
-		//move current to tail
-		move_to_tail(current);
-		return map.get(key).value;//最后return
-	}
-	private void move_to_tail(Node current){
-		current.prev=tail.prev;
-		tail.prev=current;
-		current.prev.next=current;
-		current.next=tail;
-	}
-	public void set(int key, int value){
-		if(get(key)!=-1){
-			map.get(key).value=value;
-			return;
-		}
-		
-		if(map.size()==capacity){
-			map.remove(head.next.key);
-			head.next=head.next.next;
-			head.next.prev=head;
-		}
-		
-		Node insert=new Node(key,value);
-		map.put(key, insert);
-		move_to_tail(insert);
-	}
+    private class Node{
+        Node prev;
+        Node next;
+        int key;
+        int value;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> hs = new HashMap<Integer, Node>();
+    private Node head = new Node(-1, -1);
+    private Node tail = new Node(-1, -1);
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        tail.prev = head;
+        head.next = tail;
+    }
+
+    public int get(int key) {
+	//key找不到
+        if( !hs.containsKey(key)) {   
+            return -1;
+        }
+
+        // remove current
+        Node current = hs.get(key);
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+
+        // move current to tail
+	//每次get，使用次数+1，最近使用，放于尾部
+        move_to_tail(current);			
+
+        return hs.get(key).value;
+    }
+    //数据放入缓存
+    public void set(int key, int value) {			
+        // get 这个方法会把key挪到最末端，因此，不需要再调用 move_to_tail
+        if (get(key) != -1) {
+            hs.get(key).value = value;
+            return;
+        }
+        //超出缓存上限
+        if (hs.size() == capacity) {
+	    //删除头部数据
+            hs.remove(head.next.key);		
+            head.next = head.next.next;
+            head.next.prev = head;
+        }
+
+        Node insert = new Node(key, value);		
+        hs.put(key, insert);
+        move_to_tail(insert);					
+    }
+    //移动数据至尾部
+    private void move_to_tail(Node current) { 
+        current.prev = tail.prev;
+        tail.prev = current;
+        current.prev.next = current;
+        current.next = tail;
+    }
 }
