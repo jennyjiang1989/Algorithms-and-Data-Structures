d268d24e14b2a14a7b0e33fc9e5268399589a7c7
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-06 12:02:21 -0700

    Update BackPack.java

--- DP/BackPack.java
+++ DP/BackPack.java
@@ -66,4 +66,29 @@ public class Solution {
         return 0;
     }
 }
-
+//Given n items with size nums[i] which an integer array and all positive numbers. An integer target denotes the size of a backpack. 
+//Find the number of possible fill the backpack.
+//Each item may only be used once
+public class Solution {
+    /**
+     * @param nums: an integer array and all positive numbers
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int backPackV(int[] nums, int target) {
+        // write your code here
+        //填满j的空间的方案数
+        int n=nums.length;
+        int[][] dp=new int[n+1][target+1];
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=target;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j>=nums[i-1]){
+                    dp[i][j]+=dp[i-1][j-nums[i-1]];
+                }
+            }
+        }
+        return dp[n][target];
+    }
+}
