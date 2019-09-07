d0c63a7ea750924e6035b64f96f6480fccabddc5
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-06 23:53:24 -0700

    Update BackPack.java

--- DP/BackPack.java
+++ DP/BackPack.java
@@ -215,3 +215,31 @@ public class Solution {
         return dp[amount];
     }
 }
+//Cutting a Rod
+//Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n. 
+//Determine the maximum value obtainable by cutting up the rod and selling the pieces.
+/*Input: [1, 5, 8, 9, 10, 17, 17, 20] 8 Output: 22
+Explanation:
+length   | 1   2   3   4   5   6   7   8  
+--------------------------------------------
+price    | 1   5   8   9  10  17  17  20
+by cutting in two pieces of lengths 2 and 6
+*/
+public class Solution {
+    /**
+     * @param prices: the prices
+     * @param n: the length of rod
+     * @return: the max value
+     */
+    public int cutting(int[] prices, int n) {
+        // Write your code here
+        //dp[i]表示前i英寸所能获得的最大价值。
+        int[] dp=new int[n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i]=Math.max(dp[i],dp[j]+prices[i-j-1]);
+            }
+        }
+        return dp[n];
+    }
+}
