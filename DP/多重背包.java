71e002d6ee8d1f44d8b1b62826671f1f820017ec
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-08 16:05:29 -0700

    Update 多重背包.java

--- DP/多重背包.java
+++ DP/多重背包.java
@@ -1,4 +1,7 @@
 //假设你身上有 n 元，超市里有多种大米可以选择，每种大米都是袋装的，必须整袋购买，给出每种大米的重量，价格以及数量，求最多能买多少公斤的大米
+//输入:  n = 8, prices = [3,2], weights = [300,160], amounts = [1,6]
+//输出:  640	
+//解释:  全买价格为2的米。
 public class Solution {
     /**
      * @param n: the money of you
@@ -9,10 +12,13 @@ public class Solution {
      */
     public int backPackVII(int n, int[] prices, int[] weight, int[] amounts) {
         // write your code here
-        int m = prices.length;
+        int m = prices.length;//多少种物品
         int[] f = new int[n + 1];
+        //对于每一种物品
         for(int i = 0;i < m;i++){
+            //对于每一个物品
             for(int j = 1;j <= amounts[i];j++){
+                //逆序 0-1背包 
                 for(int k = n;k >= prices[i];k--){
                     f[k] = Math.max(f[k], f[k - prices[i]] + weight[i]);
                 }
