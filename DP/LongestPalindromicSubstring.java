65ac24f27821f84e73750eba7119e935a88dce77
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-08 09:51:00 -0700

    Update LongestPalindromicSubstring.java

--- DP/LongestPalindromicSubstring.java
+++ DP/LongestPalindromicSubstring.java
@@ -2,39 +2,50 @@
 //You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
 //DP
 public class Solution {
+    /**
+     * @param s: input string
+     * @return: the longest palindromic substring
+     */
     public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) {
+        // write your code here
+        if(s==null||s.length()==0){
             return "";
         }
-        
-        int n = s.length();
-        boolean[][] isPalindrome = new boolean[n][n];
-        
-        int longest = 1, start = 0;
-        for (int i = 0; i < n; i++) {
-            isPalindrome[i][i] = true;
+        int n=s.length();
+        //table[i][j] will be true if substring str[i..j] is palindrome, else false
+        boolean[][] table=new boolean[n][n];
+        int longest=1;
+        //All substrings of length 1 are palindromes
+        for(int i=0;i<n;i++){
+            table[i][i]=true;
         }
-        for (int i = 0; i < n - 1; i++) {
-            isPalindrome[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
-            if (isPalindrome[i][i + 1]) {
-                start = i;
-                longest = 2;
+        //check for sub-string of length 2
+        int start=0;
+        //注意index
+        for(int i=0;i<n-1;i++){
+            if(s.charAt(i)==s.charAt(i+1)){
+                table[i][i+1]=true;
+                start=i;
+                longest=2;
             }
         }
-        
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i + 2; j < n; j++) {
-                isPalindrome[i][j] = isPalindrome[i + 1][j - 1] && 
-                    s.charAt(i) == s.charAt(j);
-                    
-                if (isPalindrome[i][j] && j - i + 1 > longest) {
-                    start = i;
-                    longest = j - i + 1;
+        //check for sub-string of length 3 and more
+        //k: the length of substring
+        for(int k=3;k<=n;k++){
+            //Fix the starting index 举例子
+            for(int i=0;i<=n-k;i++){
+                //calculate the endling index of substring from starting index i and length k
+                int j=i+k-1;
+                if(table[i+1][j-1]&&s.charAt(i)==s.charAt(j)){
+                    table[i][j]=true;
+                    if(k>longest){
+                        longest=k;
+                        start=i;
+                    }
                 }
             }
         }
-        
-        return s.substring(start, start + longest);
+        return s.substring(start,start+longest);
     }
 }
 //基于中心点枚举的算法，时间复杂度 O(n^2)
