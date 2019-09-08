03795dbce78baa823bd7a0493e8236516fe9dfd1
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-08 16:50:38 -0700

    Update Triangle.java

--- DP/Triangle.java
+++ DP/Triangle.java
@@ -1,4 +1,12 @@
 //Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+/* The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+[
+[2],
+[3,4],
+[6,5,7],
+[4,1,8,3]
+]
+*/
 public class Solution {
     /**
      * @param triangle: a list of lists of integers
@@ -17,7 +25,7 @@ public class Solution {
         int[][] f=new int[n][n]; 
         f[0][0]=triangle[0][0];
         
-        //initialize 三角形的左边没有右上；三角形的右边没有上
+        //initialize 三角形的左边没有左上；三角形的右边没有上
         for(int i=1;i<n;i++){
             f[i][0]=f[i-1][0]+triangle[i][0];
             f[i][i]=f[i-1][i-1]+triangle[i][i];
