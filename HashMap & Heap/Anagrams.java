1e074ec4a34157acfbc226e182fe089186fbbc77
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-10 23:20:10 -0700

    Create Anagrams.java

---
+++ HashMap & Heap/Anagrams.java
@@ -0,0 +1,32 @@
+//Given an array of strings, return all groups of strings that are anagrams.
+//Two strings are anagram if they can be the same after change the order of characters.
+
+public class Solution {
+    /**
+     * @param strs: A list of strings
+     * @return: A list of strings
+     */
+    public List<String> anagrams(String[] strs) {
+        List<String> result = new ArrayList<String>();
+        if (strs == null || strs.length == 0) {
+            return result;
+        }
+        Map<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] arr = strs[i].toCharArray(); 
+            Arrays.sort(arr);
+            String s = String.valueOf(arr);
+            if (!map.containsKey(s)) {
+                ArrayList<String> list = new ArrayList<String>();
+                map.put(s, list);
+            }
+            map.get(s).add(strs[i]);
+        } 
+        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+            if (entry.getValue().size() >= 2) {
+                result.addAll(entry.getValue());
+            }
+        }
+        return result;
+    }
+}
