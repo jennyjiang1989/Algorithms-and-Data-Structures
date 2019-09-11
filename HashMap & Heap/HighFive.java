b660a970933e83c1340cd4f8fcf27b20969ebcd6
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-11 14:08:25 -0700

    Create HighFive.java

---
+++ HashMap & Heap/HighFive.java
@@ -0,0 +1,51 @@
+//There are two properties in the node student id and scores, to ensure that each student will have at least 5 points, 
+//find the average of 5 highest scores for each person.
+//利用map与优先队列即可，找到对应的人的成绩，pop出前5个即可。
+
+/**
+ * Definition for a Record
+ * class Record {
+ *     public int id, score;
+ *     public Record(int id, int score){
+ *         this.id = id;
+ *         this.score = score;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param results a list of <student_id, score>
+     * @return find the average of 5 highest scores for each person
+     * Map<Integer, Double> (student_id, average_score)
+     */
+    public Map<Integer, Double> highFive(Record[] results) {
+        Map<Integer, Double> answer = new HashMap<Integer, Double>();
+        Map<Integer, PriorityQueue<Integer>> hash = new HashMap<Integer, PriorityQueue<Integer>>();
+
+        for (Record r : results) {
+            if (!hash.containsKey(r.id)){
+                hash.put(r.id, new PriorityQueue<Integer>());
+            }
+            PriorityQueue<Integer> pq=hash.get(r.id);
+            if (pq.size() < 5) {
+                pq.add(r.score);
+            } else {
+                if (pq.peek() < r.score){
+                    pq.poll();
+                    pq.add(r.score);
+                }
+            }
+        }
+
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : hash.entrySet()) {
+            int id = entry.getKey();
+            PriorityQueue<Integer> scores = entry.getValue();
+            double average = 0;
+            for (int i = 0; i < 5; ++i)
+                average += scores.poll();
+            average /= 5.0;
+            answer.put(id, average);
+        }
+        return answer;
+    }
+}
