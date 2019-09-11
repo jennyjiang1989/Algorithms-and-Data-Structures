5a40fe06e94bb40d231b6ed9595c3e5bc290f52b
Jenny Jiang <jiangchunxing@gmail.com>
2019-09-11 10:35:11 -0700

    Update LongestConsecutiveSequence.java

--- HashMap & Heap/LongestConsecutiveSequence.java
+++ HashMap & Heap/LongestConsecutiveSequence.java
@@ -1,4 +1,6 @@
 //Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+//Input : [100, 4, 200, 1, 3, 2] Output : 4 Explanation : The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length:4
+//o(n)
 public class Solution {
     /**
      * @param num: A list of integers
