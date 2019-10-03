12c2ea5ceb2b9f0759643c6ca20494a1b12671d5
Jenny Jiang <jiangchunxing@gmail.com>
2019-10-03 10:39:14 -0700

    Update 2Difference.java

--- Two Pointers/2Difference.java
+++ Two Pointers/2Difference.java
@@ -51,45 +51,39 @@ public class Solution {
      * @param target an integer
      * @return [index1 + 1, index2 + 1] (index1 < index2)
      */
-    public int[] twoSum7(int[] nums, int target) {
+    public int[] twoSum7(int[] A, int k) {
         // write your code here
-        int[] indexs = new int[2];
-        if (nums == null || nums.length < 2)
-            return indexs;
-
-        if (target < 0)
-            target = -target;
-
-        int n = nums.length;
-        Pair[] pairs = new Pair[n];
-        for (int i = 0; i < n; ++i)
-            pairs[i] = new Pair(i, nums[i]);
-
-        Arrays.sort(pairs, new Comparator<Pair>(){
-            public int compare(Pair p1, Pair p2){
-                return p1.num - p2.num;
-            } 
+        int[] result=new int[2];
+    	//初始化
+    	Pair[] pairs=new Pair[A.length];
+    	for(int i=0;i<A.length;i++){
+    		pairs[i]=new Pair(i,A[i]);
+    	}
+    	Arrays.sort(pairs,new Comparator<Pair>(){
+        	public int compare(Pair pair1,Pair pair2){
+        		return pair1.value-pair2.value;
+        	}
         });
-
-        int j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (i == j)
-                j ++;
-            while (j < n && pairs[j].num - pairs[i].num < target)
-                j ++;
-
-            if (j < n && pairs[j].num - pairs[i].num == target) {
-                indexs[0] = pairs[i].idx + 1;
-                indexs[1] = pairs[j].idx + 1;
-                if (indexs[0] > indexs[1]) {
-                    int temp = indexs[0];
-                    indexs[0] = indexs[1];
-                    indexs[1] = temp;
-                }
-                return indexs;
-            }
-        }
-        return indexs;
+    	int i=0;
+    	int j=0;
+    	while(i<=j&&j<A.length){
+    		if(pairs[j].value-pairs[i].value==k){
+    			if(pairs[j].index>pairs[i].index){
+    				result[0]=pairs[i].index+1;
+    				result[1]=pairs[j].index+1;
+    				return result;
+    			}else{
+    				result[0]=pairs[j].index+1;
+    				result[1]=pairs[i].index+1;
+    				return result;
+    			}
+    		}else if(pairs[j].value-pairs[i].value<k){
+    			j++;
+    		}else{
+    			i++;
+    		}
+    	}
+    	return result;
     }
 }
 
